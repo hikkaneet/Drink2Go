297742db0be231dd0ff810b5e66538bbbc336417
hikkaneet <v.y.tolokonnikov@yandex.ru>
2023-06-22 00:55:24 +0400

    Добавлен слайдер

--- source/js/script.js
+++ source/js/script.js
@@ -1,4 +1,24 @@
 /* в этот файл добавляет скрипты*/
 
 import { mobileMenu } from './mobile-menu.js';
+import './swiper-bundle.js';
+
 mobileMenu();
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+});
