773f9ad343463918a6de5de8d4323674116385be
hikkaneet <v.y.tolokonnikov@yandex.ru>
2023-06-26 20:46:29 +0400

    Добавляет правки

--- source/js/mobile-menu.js
+++ source/js/mobile-menu.js
@@ -9,11 +9,17 @@ export function mobileMenu() {
     navList.classList.toggle('main-nav__list--opened');
   };
 
+  const toggleNavToggle = () => {
+    navToggle.classList.toggle('main-nav__toggle--opened');
+  };
+
   navToggle.addEventListener('click', toggleNavList);
+  navToggle.addEventListener('click', toggleNavToggle);
 
   const checkWindowSize = () => {
     if (window.innerWidth >= 768) {
       navList.classList.remove('main-nav__list--opened');
+      navToggle.classList.remove('main-nav__toggle--opened');
     }
   };
 
